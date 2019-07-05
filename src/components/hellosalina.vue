53128eef5c09506dab3317ae2d72fd3bc951796a
“xsalina” <419593547@qq.comgit config --global user.email 419593547@qq.com>
2019-07-05 16:45:23 +0800

    增加表格到处功能

--- src/components/hellosalina.vue
+++ src/components/hellosalina.vue
@@ -2,17 +2,68 @@
   <div >
       <div @click="chooseTab">{{msg}}</div>
       <div @click="jumptologin">{{jumpto}}</div>
-
+    <!--给表格添加一个id，导出文件事件需要使用-->
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%"
+      id="out-table"
+    >
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+      >
+      </el-table-column>
+    </el-table>
+    <!--给按钮绑定事件-->
+    <button @click="exportExcel">点击导出</button>
   </div>
-  
+
 </template>
 <script>
-export default {
+  import FileSaver from "file-saver";
+  import XLSX from "xlsx";
+
+  export default {
   name: "HelloSalina",
   data() {
     return {
       msg: "Welcome to salina",
-      jumpto:'按我跳转到登录页面'
+      jumpto:'按我跳转到登录页面',
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
     };
   },
   methods:{
@@ -20,20 +71,46 @@ export default {
           this.msg = '乌拉拉了'
       },
       jumptologin(){
-           this.$router.push({  
-            path: '/login',   
-            name: 'Login',  
-            params: {   
-                key: 'key',   
-                msgKey: this.jumpto  
-            }  
-             
-        }) 
+           this.$router.push({
+            path: '/login',
+            name: 'Login',
+            params: {
+                key: 'key',
+                msgKey: this.jumpto
+            }
+
+        })
+      },
+    //定义导出Excel表格事件
+    exportExcel() {
+      /* 从表生成工作簿对象 */
+      var wb = XLSX.utils.table_to_book(document.querySelector("#out-table"));
+      /* 获取二进制字符串作为输出 */
+      var wbout = XLSX.write(wb, {
+        bookType: "xlsx",
+        bookSST: true,
+        type: "array"
+      });
+      try {
+        FileSaver.saveAs(
+          //Blob 对象表示一个不可变、原始数据的类文件对象。
+          //Blob 表示的不一定是JavaScript原生格式的数据。
+          //File 接口基于Blob，继承了 blob 的功能并将其扩展使其支持用户系统上的文件。
+          //返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+          new Blob([wbout], { type: "application/octet-stream" }),
+          //设置导出文件名称
+          "sheetjs.xlsx"
+        );
+      } catch (e) {
+        if (typeof console !== "undefined") console.log(e, wbout);
       }
-      
+      return wbout;
+    }
   }
+
+
 };
-</script>  
+</script>
 <style scoped>
 h1,
 h2 {
